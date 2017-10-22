03b454308c7a9242f7c0a29cd4e481a4b4945bcd
miksuoma <miksuoma@w7java_dev>
2017-10-22 12:01:15 +0300

    x

--- JQuickClient/src/org/quickclient/actions/Delete.java
+++ JQuickClient/src/org/quickclient/actions/Delete.java
@@ -1,6 +1,5 @@
 package org.quickclient.actions;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
@@ -27,7 +26,6 @@ import com.documentum.operations.IDfOperationNode;
 public class Delete implements IQuickAction {
 
 	private List<String> idlist;
-<<<<<<< HEAD
 	private JTable table;
 
 	private boolean deleteFolder(final IDfSysObject obj) throws DfException {
@@ -77,41 +75,6 @@ public class Delete implements IQuickAction {
 		}
 		table.validate();
 
-=======
-	private final List<String> deletedidlist = new ArrayList<>();
-	private JTable table;
-
-	private boolean deleteFolder(final IDfSysObject obj) throws DfException {
-		boolean success = false;
-		final int answer2 = JOptionPane.showConfirmDialog(null, "Selected object '" + obj.getObjectName() + "' is folder, should all contents be deleted??!", "Confirm", JOptionPane.YES_NO_OPTION);
-		if (answer2 == JOptionPane.YES_OPTION) {
-			final IDfClientX clientx = new DfClientX();
-			final IDfDeleteOperation deleop = clientx.getDeleteOperation();
-			deleop.setVersionDeletionPolicy(IDfDeleteOperation.ALL_VERSIONS);
-			deleop.enableDeepDeleteFolderChildren(false);
-			deleop.enableDeepDeleteVirtualDocumentsInFolders(false);
-			deleop.setOperationMonitor(new OperationMonitor());
-			deleop.setDeepFolders(true);
-			deleop.add(obj);
-			success = deleop.execute();
-			final IDfList errors = deleop.getErrors();
-			if (errors.getCount() > 0) {
-				success = false;
-			}
-		} else {
-			final IDfClientX clientx = new DfClientX();
-			final IDfDeleteOperation deleop = clientx.getDeleteOperation();
-			deleop.setOperationMonitor(new OperationMonitor());
-			deleop.setDeepFolders(false);
-			deleop.add(obj);
-			success = deleop.execute();
-			final IDfList errors = deleop.getErrors();
-			if (errors.getCount() > 0) {
-				success = false;
-			}
-		}
-		return success;
->>>>>>> branch 'master' of https://github.com/MikaSu/QuickClient.git
 	}
 
 	@Override
@@ -122,22 +85,12 @@ public class Delete implements IQuickAction {
 		try {
 			session = smanager.getSession();
 			for (final String objid : idlist) {
-<<<<<<< HEAD
-=======
-				// for (int i = 0; i < idlist.size(); i++) {
-				// final String objid = idlist.get(i);
->>>>>>> branch 'master' of https://github.com/MikaSu/QuickClient.git
+
 				final IDfSysObject obj = (IDfSysObject) session.getObject(new DfId(objid));
 				if (objid.startsWith("0b") || objid.startsWith("0c")) {
 					final int answer2 = JOptionPane.showConfirmDialog(null, "Delete Folder " + obj.getObjectName() + " ?", "Choose", JOptionPane.YES_NO_OPTION);
 					if (answer2 == JOptionPane.YES_OPTION) {
-<<<<<<< HEAD
 						deleteFolder(obj);
-=======
-						if (deleteFolder(obj)) {
-							deletedidlist.add(objid);
-						}
->>>>>>> branch 'master' of https://github.com/MikaSu/QuickClient.git
 					}
 				} else {
 					final IDfClientX clientx = new DfClientX();
@@ -152,31 +105,15 @@ public class Delete implements IQuickAction {
 					deleop.setOperationMonitor(new OperationMonitor());
 					deleop.execute();
 					final IDfList errors = deleop.getErrors();
-<<<<<<< HEAD
+
 					if (errors != null && errors.getCount() == 0) {
 
 						deleteFromTable(obj.getObjectId().getId());
-=======
-					if (errors.getCount() == 0) {
-						deletedidlist.add(objid);
->>>>>>> branch 'master' of https://github.com/MikaSu/QuickClient.git
+
 					}
 				}
 			}
-<<<<<<< HEAD
 
-=======
-			final int rowcount = table.getRowCount();
-			final DefaultTableModel tablemodel = (DefaultTableModel) table.getModel();
-			for (int i = 0; i < rowcount; i++) {
-				final Vector v = (Vector) tablemodel.getDataVector().elementAt(i);
-				final DokuData d = (DokuData) v.lastElement();
-				if (this.deletedidlist.contains(d.getObjID())) {
-					tablemodel.removeRow(i);
-				}
-			}
-			table.validate();
->>>>>>> branch 'master' of https://github.com/MikaSu/QuickClient.git
 		} catch (final DfException ex) {
 			DfLogger.error(this, ex.getMessage(), null, ex);
 			SwingHelper.showErrorMessage("Error occurred!", ex.getMessage());
